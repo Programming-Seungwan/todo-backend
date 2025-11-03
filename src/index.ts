d0e5f362ea579df69ce6a56db3109ce3e4e208f0
Programming-Seungwan <swsj4480@gmail.com>
2025-11-03 23:54:06 +0900

    chore : esm 방식의 프로젝트를 위해 tsx 라이브러리를 사용하고 connection 성공

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
 import "dotenv/config";
 import type { Request, Response } from "express";
 import express from "express";
+import pool from "./database/connection.js";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.get("/", async (req: Request, res: Response) => {
+  try {
+    const connection = await pool.getConnection();
+    const [rows, fields] = await connection.execute("select * from Users");
+    console.log(fields, "fields");
+    res.status(200).json({
+      success: true,
+      data: rows,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.listen(port, () => {
