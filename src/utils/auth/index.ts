a5d97e8f47470aca938f5691208435cf6e3fe6d2
Programming-Seungwan <swsj4480@gmail.com>
2025-11-10 22:48:45 +0900

    fix : 회원 가입을 bcrypt 모듈을 통해 비밀번호를 해싱하는 방식으로 수정

--- src/utils/auth/index.ts
+++ src/utils/auth/index.ts
@@ -2,8 +2,8 @@ import bcrypt from "bcrypt";
 
 const SALT_ROUNDS = 12;
 
-export async function hashPassword(plainPassword: string) {
-  return await bcrypt.hash(plainPassword, SALT_ROUNDS);
+export function hashPassword(plainPassword: string) {
+  return bcrypt.hash(plainPassword, SALT_ROUNDS);
 }
 
 export async function verifyPassword(
